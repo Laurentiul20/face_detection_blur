9b41c2c166ad06d9e2947c9635d22f6f5adfa54e
unknown <shubhamdave70@gmail.com>
2022-11-12 23:05:19 -0500

    adding input logic

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1>Face detection and blurring</h1>
+
+<p>This project will take either an image or a video as input and apply processing techniques to detect the faces in the media file and blur them.</p>
+
+<h2>Usage:</h2>
+<p>Pass the file path as command line parameter. For example, if an image file called <i>test_img.jpeg</i> exists in <i>input_files</i> folder, then run the following command to run the program:</p>
+
+<p><code>python face_blurring.py input_files/test_img.jpeg</code><p>
--- face_blurring.py
+++ face_blurring.py
@@ -1,20 +1,58 @@
 import cv2
+import sys
+import os
 
-# load the input image and convert it to grayscale
-image = cv2.imread("images/test_img.jpeg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# storing path of input file in a variable
+path = sys.argv[1]
 
-# load the face detector
-face_detector = cv2.CascadeClassifier("lib/haarcascade_frontalface_default.xml")
 
-# detect the faces in the grayscale image
-face_rects = face_detector.detectMultiScale(gray, 1.14, 6, minSize=(65, 65))
+def img_processing(path):
+    # load the input image and convert it to grayscale
+    image = cv2.imread(path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# go through the face bounding boxes 
-for (x, y, w, h) in face_rects:
-    
-    # draw a rectangle around the face on the input image
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+    # load the face detector
+    face_detector = cv2.CascadeClassifier(
+        "lib/haarcascade_frontalface_default.xml")
+
+    # detect the faces in the grayscale image
+    face_rects = face_detector.detectMultiScale(
+        gray, 1.14, 6, minSize=(65, 65))
+
+    # go through the face bounding boxes
+    for (x, y, w, h) in face_rects:
+
+        # draw a rectangle around the face on the input image
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def vid_processing():
+    pass
+
+
+def main():
+    # list of image and video type extensions
+    vid_extensions = ['.MP4', '.M4P', '.M4V', '.MPG', '.MP2', '.MPEG', '.MPE',
+                      '.MPV', '.MOV', '.QT', '.AVI', '.WMV', '.FLV', '.SWF', '.WEBM', '.OGG']
+    img_extensions = ['.JPG', '.PNG', '.GIF', '.WEBP', '.TIFF', '.PSD',
+                      '.RAW', '.BMP', '.HEIF', '.INDD', '.JPEG', '.SVG', '.AI', '.EPS', '.PDF']
+
+    # splitting path into file name and extension to get the extension
+    split_path = os.path.splitext(path)
+    path_ext = split_path[1]
+
+    # if path is an image, redirect to image processing, otherwise redirect to video logic
+    if path_ext.upper() in img_extensions:
+        img_processing(path)
+    elif path_ext.upper() in vid_extensions:
+        vid_processing(path)
+    else:
+        print('Invalid file type! Make sure the file is an image or a video file')
+
+
+if __name__ == "__main__":
+    main()
